04c184850d9cab36b543878fa80ee9f71a0a5d61
Bavisetti Pavan Kalyan <135210660+Pavan-kalyan99@users.noreply.github.com>
2023-09-26 13:03:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,22 +17,30 @@ import customers from './models/userModel.js'
 import JWT from 'jsonwebtoken';
 import { hashPassword } from './helpers/authHelper.js';
 //path
+//CYCLIC
 import path from 'path';
 import {fileURLToPath} from 'url'
 
 const app=express()
 //configure port
 dotenv.config();
+//DB connect
+connectDB();
+
+
 //middleware
 //app.use(cors());
 app.use(cors({origin:"*"}))
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //cyclic
 app.use(express.static(path.join(__dirname,'./client/build')))
 
-//DB connect
-connectDB();
+
+
 
 //====view engine=======
 app.set("view engine","ejs");
